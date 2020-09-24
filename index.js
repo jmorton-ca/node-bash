4dd0605de1d32e0f83fd48d81575ed6d114bdc19
James Morton <james_morton@brown.edu>
2020-09-24 09:57:12 -0400

    Basic bash command

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-// App Entrypoint
-console.log("App ran!");
+const bash = require('child_process');
+
+const cmds = {
+  ls: "ls"
+}
+
+bash.exec(cmds.ls, (err, stdout, stderr) => {console.log(stdout)});
